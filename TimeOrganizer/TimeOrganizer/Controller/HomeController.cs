c706a4be0ce1e420032b5783a65a69469bc425fa
nciganovic <nciganovic99@gmail.com>
2020-10-18 01:12:24 +0200

    Show SchoolType on Index page

---
+++ TimeOrganizer/TimeOrganizer/Controller/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeOrganizer.Model;
+using TimeOrganizer.Model.InterfaceRepo;
+
+namespace TimeOrganizer.Controller
+{
+    public class HomeController
+    {
+        private ISchoolTypeRepository schoolTypeRepository;
+
+        public HomeController(ISchoolTypeRepository schoolTypeRepository)
+        {
+            this.schoolTypeRepository = schoolTypeRepository;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public JsonResult Index() {
+            IEnumerable<SchoolType> allSchoolTypes = schoolTypeRepository.AllSchoolTypes();
+            JsonResult jsonResult = new JsonResult(allSchoolTypes);
+            return jsonResult;
+        }
+    }
+}
---
+++ TimeOrganizer/TimeOrganizer/Model/InterfaceRepo/ISchoolTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeOrganizer.Model.InterfaceRepo
+{
+    public interface ISchoolTypeRepository
+    {
+        public IEnumerable<SchoolType> AllSchoolTypes();
+    }
+}
