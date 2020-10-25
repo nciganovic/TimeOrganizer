0826a01a90abd25ceab57e57ea02c74dff2624c1
nciganovic <nciganovic99@gmail.com>
2020-10-25 11:02:05 +0100

    Create repository for color and task

---
+++ TimeOrganizer/TimeOrganizer/Model/SqlRepository/SqlColorRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeOrganizer.Model.InterfaceRepo;
+using TimeOrganizer.Model.Tables;
+
+namespace TimeOrganizer.Model.SqlRepository
+{
+    public class SqlColorRepository : IColorRepository
+    {
+        private AppDbContext appDbContext;
+
+        public SqlColorRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public Color GetColorById(int id)
+        {
+            var color = appDbContext.Colors.Find(id);
+            return color;
+        }
+
+    }
+}
---
+++ TimeOrganizer/TimeOrganizer/Model/SqlRepository/SqlTaskTypeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeOrganizer.Model.InterfaceRepo;
+using TimeOrganizer.Model.Tables;
+
+namespace TimeOrganizer.Model.SqlRepository
+{
+    public class SqlTaskTypeRepository : ITaskTypeRepository
+    {
+        private AppDbContext appDbContext;
+
+        public SqlTaskTypeRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public TaskType GetTaskTypeById(int id)
+        {
+            var taskType = appDbContext.TaskTypes.Find(id);
+            return taskType;
+        }
+    }
+}
