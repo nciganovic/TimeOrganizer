a896be3b4b372cf58fa2052cc5c69536521d32c1
nciganovic <nciganovic99@gmail.com>
2020-11-06 16:57:30 +0100

    Create UpdateUserRoles method in AdminController

--- TimeOrganizer/TimeOrganizer/Controllers/AdminController.cs
+++ TimeOrganizer/TimeOrganizer/Controllers/AdminController.cs
@@ -5,6 +5,8 @@ using TimeOrganizer.Model.Tables;
 using TimeOrganizer.ViewModel;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace TimeOrganizer.Controllers
 {
@@ -13,10 +15,12 @@ namespace TimeOrganizer.Controllers
     public class AdminController : Controller
     {
         private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUser> userManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpPost]
@@ -38,6 +42,42 @@ namespace TimeOrganizer.Controllers
 
             var invalidModelStateErrror = ModelState.Select(x => x.Value.Errors).Where(y => y.Count() > 0).ToList();
             return new JsonResult(new { errors = invalidModelStateErrror }) ;
+        }
+
+        [HttpPost]
+        [Route("users/roles/update")]
+        public async Task<IActionResult> UpdateUserRoles(List<string> roles, string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+
+            if (user != null)
+            {
+                //Delete all current roles
+                var allRoles = await roleManager.Roles.ToListAsync();
+                foreach (var role in allRoles) {
+                    var result = await userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+
+                foreach (var role in roles) {
+                    var result = await userManager.AddToRoleAsync(user, role);
+                    
+                    if (!result.Succeeded) {
+                        ModelState.AddModelError(string.Empty, $"Failed to add {role} to user");                    
+                    }
+                }
+                
+            }
+            else {
+                ModelState.AddModelError(string.Empty, $"User with id {userId} cannot be found.");
+            }
+
+            var invalidModelStateError = ModelState.Select(x => x.Value.Errors).Where(y => y.Count() > 0).ToList();
+
+            if (invalidModelStateError.Count == 0) {
+                return new JsonResult(new { message = "Roles successfully added." });
+            }
+
+            return new JsonResult(new { errors = invalidModelStateError });
         }
     }
 }
