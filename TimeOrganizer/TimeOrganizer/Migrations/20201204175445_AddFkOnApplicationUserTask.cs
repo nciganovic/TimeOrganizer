dcbc6ba56c572f684431ace9a7dd3b6c8ab90c4f
nciganovic <nciganovic99@gmail.com>
2020-12-04 20:01:54 +0100

    add foriegn key on ApplicationUserTask table

---
+++ TimeOrganizer/TimeOrganizer/Migrations/20201204175445_AddFkOnApplicationUserTask.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeOrganizer.Migrations
+{
+    public partial class AddFkOnApplicationUserTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RelationshipStatusId",
+                table: "ApplicationUserTask",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserTask_RelationshipStatusId",
+                table: "ApplicationUserTask",
+                column: "RelationshipStatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApplicationUserTask_RelationshipStatuses_RelationshipStatusId",
+                table: "ApplicationUserTask",
+                column: "RelationshipStatusId",
+                principalTable: "RelationshipStatuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApplicationUserTask_RelationshipStatuses_RelationshipStatusId",
+                table: "ApplicationUserTask");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ApplicationUserTask_RelationshipStatusId",
+                table: "ApplicationUserTask");
+
+            migrationBuilder.DropColumn(
+                name: "RelationshipStatusId",
+                table: "ApplicationUserTask");
+        }
+    }
+}
--- TimeOrganizer/TimeOrganizer/Model/Tables/ApplicationUserTask.cs
+++ TimeOrganizer/TimeOrganizer/Model/Tables/ApplicationUserTask.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TimeOrganizer.Model.Tables;
 
 namespace TimeOrganizer.Model.Tables
 {
@@ -11,5 +12,7 @@ namespace TimeOrganizer.Model.Tables
         public ApplicationUser ApplicationUser { get; set; }
         public int TaskId { get; set; }
         public Task Task { get; set; }
+        public int RelationshipStatusId { get; set; }
+        public RelationshipStatus RelationshipStatus { get; set; }
     }
 }
